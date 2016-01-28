6e70ee1b7ecd0dca924d32f788d222de6dac1259
Ashwin Nanjappa <ashwin.n@gmail.com>
2016-01-28 15:02:37 +0800

    Fixed typo in license.

--- GDelFlipping/src/gDel3D/GPU/KerPredicates.h
+++ GDelFlipping/src/gDel3D/GPU/KerPredicates.h
@@ -23,7 +23,7 @@ conditions and the following disclaimer. Redistributions in binary form must rep
 the above copyright notice, this list of conditions and the following disclaimer
 in the documentation and/or other materials provided with the distribution. 
 
-Neither the name of the National University of University nor the names of its contributors
+Neither the name of the National University of Singapore nor the names of its contributors
 may be used to endorse or promote products derived from this software without specific
 prior written permission from the National University of Singapore. 
 
