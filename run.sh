744ed29621a934cbc7691d152a734b6a51d253d3
Pourya Shirazian <Pourya.Shirazian@gmail.com>
2018-07-31 11:23:48 -0700

    fixed the install prefix path

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ fi
 mkdir -p ${BUILD_TREE}
 pushd ${BUILD_TREE}
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCUDA_TOOLKIT_ROOT_DIR=/devsystem/dependencies/ubuntu16/cuda/cuda-8.0/ -DCMAKE_POSITION_INDEPENDENT_CODE=True --DCMAKE_INSTALL_PREFIX=/devsystem/dependencies/ubuntu16/gflip3d/gflip3d-1.0/ ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCUDA_TOOLKIT_ROOT_DIR="/devsystem/dependencies/ubuntu16/cuda/cuda-8.0/" -DCMAKE_POSITION_INDEPENDENT_CODE=True --DCMAKE_INSTALL_PREFIX="/devsystem/dependencies/ubuntu16/gflip3d/gflip3d-1.0/" ..
 
 make -j
 make install
