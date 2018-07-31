00e420f8187cacc24a55b5545ea466f801aa8a30
Pourya Shirazian <Pourya.Shirazian@gmail.com>
2018-07-31 11:22:34 -0700

    fixed issues in the run script

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-build-dir="build-release"
-mkdir ${build-dir}
-
-pushd ${build-dir}
+BUILD_TREE="build-release"
+if [ -d ${BUILD_TREE} ]; then
+	rm -rf ${BUILD_TREE}
+fi
+	
+mkdir -p ${BUILD_TREE}
+pushd ${BUILD_TREE}
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCUDA_TOOLKIT_ROOT_DIR=/devsystem/dependencies/ubuntu16/cuda/cuda-8.0/ -DCMAKE_POSITION_INDEPENDENT_CODE=True --DCMAKE_INSTALL_PREFIX=/devsystem/dependencies/ubuntu16/gflip3d/gflip3d-1.0/ ..
 
